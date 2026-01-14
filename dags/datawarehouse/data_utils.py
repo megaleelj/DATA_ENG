79728de466800af10b505e9b84e49b508709617d
Osemeigah Igbafe <mega4lj@gmail.com>
2026-01-14 17:49:07 +0000

    I have improve the code, createing the full ELT pipe runing and also gone through test

---
+++ dags/datawarehouse/data_utils.py
@@ -0,0 +1,63 @@
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from psycopg2.extras import RealDictCursor
+
+table = "yt_api"
+
+def get_conn_cursor():
+    hook = PostgresHook(postgres_conn_id="postgres_db_yt_elt", schema="elt_db")
+    conn = hook.get_conn()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    return conn, cur
+
+def close_conn_cursor(conn, cur):
+    cur.close()
+    conn.close()
+
+def create_schema(schema):
+    conn, cur = get_conn_cursor()
+    schema_sql = f"CREATE SCHEMA IF NOT EXISTS {schema};"
+    cur.execute(schema_sql)
+    conn.commit()
+    close_conn_cursor(conn, cur)
+
+def create_table(schema):
+    conn, cur = get_conn_cursor()
+
+    if schema == "staging":
+        table_sql = f"""
+            CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                "Video_id" VARCHAR(11) PRIMARY KEY,
+                "Video_title" TEXT NOT NULL,
+                "Upload_date" TIMESTAMP NOT NULL,
+                "Duration" VARCHAR(20) NOT NULL,
+                "Video_views" INT,
+                "Likes_Count" INT,
+                "Comments_count" INT
+            );
+        """
+    else:
+        table_sql = f"""
+            CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                "Video_ID" VARCHAR(11) PRIMARY KEY,
+                "Video_Title" TEXT NOT NULL,
+                "Upload_date" TIMESTAMP NOT NULL,
+                "Duration" VARCHAR(20) NOT NULL,
+                "Video_Type" VARCHAR(20) NOT NULL,
+                "Video_views" INT,
+                "Likes_Count" INT,
+                "Comments_Count" INT
+            );
+        """
+
+    cur.execute(table_sql)
+    conn.commit()
+    close_conn_cursor(conn, cur)
+
+def get_video_ids(cur, schema):
+    column = "Video_id" if schema == "staging" else "Video_ID"
+
+    cur.execute(f'''SELECT "{column}" FROM {schema}.{table};''')
+    ids = cur.fetchall()
+
+    video_ids = [row[column] for row in ids]
+    return video_ids
